722aeea97a47ac88abd7614279d4843f6dce1c25
mahibashardeveloper <mahibashar2023@gmail.com>
2023-10-26 10:37:56 +0600

    include lightbox

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,10 @@
     <title>Photography</title>
     <link rel="shortcut icon" href="{{asset('/images/logo.png')}}" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.4/js/lightbox.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.4/js/lightbox-plus-jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.4/css/lightbox.min.css">
 </head>
 <body>
     <div id="app">
